2f4f2d85cd9b2b13cde2dcaf1e80cc1f056f3ae8
Mohammad Shahid <www.shahid6516@gmail.com>
2025-11-11 14:13:33 +0530

    Implemented Role based access for the admin , manager and users

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,23 +1,24 @@
 import express from "express";
 import { verifyToken } from "../middleware/authMiddleware.js";
+import {authorizeRole} from "../middleware/roleMiddleware.js"
 
 const router = express.Router();
 
 // only admin can access this route
 
-router.get("/admin", verifyToken, (req, res) => {
+router.get("/admin", verifyToken,authorizeRole("admin"), (req, res) => {
   res.json({ message: "Welcome admin" });
 });
 
 // both admin and manager can access this route
 
-router.get("/manager", verifyToken, (req, res) => {
+router.get("/manager", verifyToken,authorizeRole("admin","manager"), (req, res) => {
   res.json({ message: "Welcome manager" });
 });
 
 // all can access this route
 
-router.get("/user", verifyToken, (req, res) => {
+router.get("/user", verifyToken,authorizeRole("admin", "manager", "user"), (req, res) => {
   res.json({ message: "Welcome user" });
 });
 
