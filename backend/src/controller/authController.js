50223ed1f4f5df3aae0488d2eae9e5806c106bda
Mohammad Shahid <www.shahid6516@gmail.com>
2025-12-09 12:49:32 +0530

    RBAC

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -38,11 +38,11 @@ export const login = async (req, res) => {
     }
 
     const token = jwt.sign(
-      
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
+    
 
     res.status(200).json({
       message: "Login successfully",
