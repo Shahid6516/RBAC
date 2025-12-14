538db4b966b232a3caeade17f5df050603f87a4b
Mohammad Shahid <www.shahid6516@gmail.com>
2025-12-14 23:16:26 +0530

    RBAC

--- backend/src/middleware/roleMiddleware.js
+++ backend/src/middleware/roleMiddleware.js
@@ -2,7 +2,6 @@ export const authorizeRole = (...allowedRoles) => {
   return (req, res, next) => {
     if (!allowedRoles.includes(req.user.role)) {
       return res.status(403).json({
-        
         message: "Access Denied",
       });
     }
